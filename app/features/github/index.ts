199426aabf2c4be85ef41763134e4f0d6c31f8ab
Bruno Padilha Rangel <brpadilha.dev@gmail.com>
2022-04-01 11:47:20 -0300

    refactor: create an index for feature/github

---
+++ app/features/github/index.ts
@@ -0,0 +1,3 @@
+export * from './GithubContainer'
+export * from './types'
+export * as GithubApi from './api'
--- app/routes/github.$username.tsx
+++ app/routes/github.$username.tsx
@@ -1,15 +1,14 @@
 import { LoaderFunction, useLoaderData } from "remix";
-import { GithubContainer } from "~/features/github/GithubContainer";
-import { LoaderData } from "~/features/github/types";
-import { getGithubUser } from '../features/github/api';
+import { GithubApi, GithubContainer, LoaderData } from "~/features/github";
+
 
 
 // Loader function
 // it will load data from github api and can access like a route on http://localhost:3000/github/$username
 export const loader: LoaderFunction = async ({params})=>{
   return {
-    user: await getGithubUser(params.username),
-  }
+    user: await GithubApi.getGithubUser(params.username),
+  };
 }
 
 export default function(){
